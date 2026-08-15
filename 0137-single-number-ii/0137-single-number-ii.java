80fadd0bed39c0a39b1f0b5e9f4aad648f4de895
Niladri Santra <157535290+Niladri035@users.noreply.github.com>
2026-08-15 08:53:45 +0530

    Time: 1 ms (92.82%), Space: 45.2 MB (63.02%) - LeetHub

--- 0137-single-number-ii/0137-single-number-ii.java
+++ 0137-single-number-ii/0137-single-number-ii.java
@@ -1,23 +1,12 @@
 class Solution {
     public int singleNumber(int[] nums) {
+        int ones = 0;  
+        int twos = 0;  
 
-        int ans = 0;
-
-        for (int i = 0; i < 32; i++) {
-
-            int count = 0;
-
-            for (int num : nums) {
-                if ((num & (1 << i)) != 0) {
-                    count++;
-                }
-            }
-
-            if (count % 3 != 0) {
-                ans |= (1 << i);
-            }
+        for (int num : nums) {
+            ones = (ones ^ num) & ~twos;
+            twos = (twos ^ num) & ~ones;
         }
-
-        return ans;
+        return ones;
     }
 }
